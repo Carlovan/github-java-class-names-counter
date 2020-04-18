a323a9d1562d9dae8a9094cad53c7920b275c10e
Carlovan <giulio.carlassare@gmail.com>
2020-04-18 15:54:35 +0200

    Some fixes

--- src/main/kotlin/github/Helpers.kt
+++ src/main/kotlin/github/Helpers.kt
@@ -1,14 +1,10 @@
 package github
 
 import com.beust.klaxon.Converter
-import com.beust.klaxon.JsonObject
 import com.beust.klaxon.JsonValue
-import com.beust.klaxon.Klaxon
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.io.Reader
 import java.net.HttpURLConnection
 import java.time.Instant
-import java.util.*
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -16,7 +12,7 @@ val dateConverter = object: Converter {
     override fun canConvert(cls: Class<*>)
         = cls == Instant::class.java
 
-    override fun toJson(value: Any): String = "\"${(value as Instant)}\""
+    override fun toJson(value: Any): String = "\"${value as Instant}\""
 
     override fun fromJson(jv: JsonValue) = Instant.parse(jv.string)
 
@@ -39,8 +35,11 @@ class PaginatedRequest(path: String) {
     }
 }
 
+/**
+ * This is useful when loading from the cache
+ */
 class LazyWithSetter<R, T>(val initializer: () -> T ) : ReadWriteProperty<R, T> {
-    var propValue: T? = null
+    private var propValue: T? = null
 
     override fun getValue(thisRef: R, property: KProperty<*>): T {
         if (propValue == null) {
