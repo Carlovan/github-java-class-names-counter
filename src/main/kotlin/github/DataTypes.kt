121578368975a3205545fbfce1f38495bcb8e29e
Carlovan <giulio.carlassare@gmail.com>
2020-04-18 15:52:56 +0200

    Better check for Java repositories

--- src/main/kotlin/github/DataTypes.kt
+++ src/main/kotlin/github/DataTypes.kt
@@ -14,9 +14,12 @@ data class Repository(
     @Json(name="full_name") val name: String,
     val languages_url: String) {
     var isJava: Boolean by LazyWithSetter{
+        val topLanguagesCount = 2
         try {
             val conn = GithubConnector.requestUrl(languages_url)
-            Klaxon().parseJsonObject(conn.reader).containsKey("Java")
+            val parsed = Klaxon().parseJsonObject(conn.reader)
+            val topLanguages = parsed.keys.sortedByDescending { key -> parsed.long(key) }.take(topLanguagesCount)
+            topLanguages.map { it.toLowerCase() }.contains("java")
         } catch (e: IOException) {
             false
         }
